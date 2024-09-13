ddc545dc854ae6769d64c95f2a382e71678b1bb5
FnaRZ <fnarzgg@mail.ru>
2024-09-13 14:49:29 +0500

    master

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class MyTasks(models.Model):
     task_name = models.CharField('Название задачи',max_length=150,default='NoNameTask')
     task_desc = models.CharField('Описание задачи',max_length=512,default='No descriptions')
     task_complited = models.BooleanField('Задача выполнена',default=False)
-
+    task_by_user = models.ForeignKey(User,on_delete=models.PROTECT)
     def __str__(self):
         return self.task_name
     class Meta:
--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.task,name="task")
+    path('',views.task,name="task"),
+    # path('/<int:id>/')
 ]
