1ff218ffc62f30fd7997f9eea9e8dd940477d003
FnaRZ <fnarzgg@mail.ru>
2024-09-16 17:35:27 +0500

    master

--- todolist/user/views.py
+++ todolist/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
 def userprofile(request):
-    return render(request,'user/defaultuserprofile.html')
+    user = User.objects.all()
+    return render(request,'user/defaultuserprofile.html',{'user':user})
